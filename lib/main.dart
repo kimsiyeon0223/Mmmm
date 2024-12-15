b04d11c2d0b7bf09b62a5a28151f23d860076b75
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-12-15 22:56:24 +0900

    자산 페이지와 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/property.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,67 +12,81 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const SizedBox(height: 100),
-            Column(
-              children: const [
-                Text(
-                  'Mmmm',
-                  style: TextStyle(
-                    fontFamily: 'Itim',
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(height: 100),
+          Column(
+            children: const [
+              Text(
+                'Mmmm',
+                style: TextStyle(
+                  fontFamily: 'Itim',
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
-                SizedBox(height: 2),
-                Text(
-                  'My money\nmy management',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Itim',
-                    fontSize: 20,
-                    color: Colors.grey,
-                    height: 1.2,
-                  ),
+              ),
+              SizedBox(height: 2),
+              Text(
+                'My money\nmy management',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Itim',
+                  fontSize: 20,
+                  color: Colors.grey,
+                  height: 1.2,
                 ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 40.0),
-              child: SizedBox(
-                width: double.infinity,
-                height: 60,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // 버튼 클릭 시 동작
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF775AF4),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 40.0),
+            child: SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const PropertyScreen(),
                       ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF775AF4),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                    child: const Text(
-                      '시작하기',
-                      style: TextStyle(
-                        fontFamily: 'Pretendard',
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
+                  ),
+                  child: const Text(
+                    '시작하기',
+                    style: TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontSize: 20,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
