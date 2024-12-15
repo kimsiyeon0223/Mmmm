ad76baaa7ad1fec8e5eca0098ae56c7f25ece42d
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-12-15 22:07:26 +0900

    랜딩페이지 퍼블리싱

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,68 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(height: 100),
+            Column(
+              children: const [
+                Text(
+                  'Mmmm',
+                  style: TextStyle(
+                    fontFamily: 'Itim',
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 2),
+                Text(
+                  'My money\nmy management',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Itim',
+                    fontSize: 20,
+                    color: Colors.grey,
+                    height: 1.2,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 버튼 클릭 시 동작
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF775AF4),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      '시작하기',
+                      style: TextStyle(
+                        fontFamily: 'Pretendard',
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
